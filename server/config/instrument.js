187b1d50b73c0ead8e949e999efe75f0f889aaea
Nikhil <nikhil.sing4141@gmail.com>
2025-06-13 15:34:27 +0530

    First Commit

---
+++ server/config/instrument.js
@@ -0,0 +1,12 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+import * as Sentry from "@sentry/node"
+
+Sentry.init({
+  dsn: "https://42f26187f26bd7c431368f1cc75f3f45@o4509485130317829.ingest.us.sentry.io/4509485135953920",
+
+  integrations: [Sentry.mongooseIntegration()],
+
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
