cec069cd29b966632dda121c6e52a9ee74e1a73e
Nikhil <nikhil.sing4141@gmail.com>
2025-10-06 01:33:48 +0530

    typo error

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 app.post('/webhooks', clerkWebhooks)
 app.use('/api/company', companyRoutes)
 app.use('/api/jobs', jobRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/users', userRoutes)
 
 // Port
 const PORT = process.env.PORT || 5000
