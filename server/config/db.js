676077ee6249ec9ead21ff77229a923af80b60a6
Nikhil <nikhil.sing4141@gmail.com>
2025-10-06 02:47:07 +0530

    updated db

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,28 @@
 import mongoose from "mongoose";
 
-// Function to connect to the MongoDb database
+let isConnected = false; // track the connection
+
 const connectDB = async () => {
+  if (isConnected) {
+    // console.log("MongoDB already connected");
+    return;
+  }
 
-    mongoose.connection.on('connected', () => console.log('Database Connected'))
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    await mongoose.connect(process.env.MONGODB_URI)
+    isConnected = db.connections[0].readyState === 1;
 
-}
+    if (isConnected) {
+      console.log("✅ Database Connected");
+    }
+  } catch (error) {
+    console.error("❌ Database connection error:", error);
+    throw new Error("Database connection failed");
+  }
+};
 
-export default connectDB
+export default connectDB;
